96254f0fd4ef5ec067cbebe1930f0c5a32f6e491
ddimitrov <ddimitrov_interopion@bitbucket.org>
2018-03-01 14:05:29 +0200

    SNDBX-472 1. Added search to the patient list 2. Changed the display of the names (now showing full name) 3. Formatted the birth date as per spec (DD MMM YYYY)

--- src/MyAwesomeReactComponent.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-
-const MyAwesomeReactComponent = () => (
-    <h1>Doctor</h1>
-);
-
-export default MyAwesomeReactComponent;
