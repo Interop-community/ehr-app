debb04cd4e80939f71c05b974794f33d6abe73ec
dimitar <dimitar@interopion.com>
2019-12-02 11:44:32 +0200

    Test for IE11

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import 'react-app-polyfill/stable';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'react-app-polyfill/ie11';
 import "@babel/polyfill";
 
 ReactDOM.render(<App />, document.getElementById('root'));
